184fabf3bb53bef58cf477d0e39606b98d2f8a62
dravog7 <dravog77@gmail.com>
2020-09-09 15:18:32 +0530

    pagespeed 100

--- src/components/Table.svelte
+++ src/components/Table.svelte
@@ -1,99 +1,12 @@
 <script>
 import {socketStore} from "../store/websocket.js";
-import Choose from "./Choose.svelte";
-export let h;
-export let w;
 
-let board = [];
-let You = ""
-let From = null
-let To = null
-let choice = null
-for(let i=0;i<h;i++){
-    let row = []
-    for(let j=0;j<w;j++){
-        row.push({Value:0,Owner:""})
-    }
-    board.push(row)
-}
 
-$: if($socketStore){
-    if($socketStore.Board)
-        setBoard($socketStore.Board.Board)
-    You = $socketStore.You
-}
-
-function setBoard(boardData){
-    From = null
-    To = null
-    if(!boardData)
-        return
-    for(let i=0;i<h;i++){
-        for(let j=0;j<w;j++){
-            board[i][j] = boardData[i][j]
-        }
-    }
-}
-
-async function tdClick(i,j){
-    let cell = board[i][j]
-    if($socketStore.Next!=$socketStore.You)
-        return
-    if(cell.Value==-1)
-        return
-    if(!From){
-        if(cell.Owner!=You)
-            return
-        if(cell.Value==0){
-            From=[i,j]
-            let val = await getChoice()
-            if(val=="X"){
-                From = null
-                return
-            }
-            board[i][j].Value = Number(val)
-            addMove()
-            return
-        }
-        From = [i,j]
-        console.log("in !From")
-        return
-    }else{
-        if((cell.Owner==You)&&(cell.Value>0))
-        return
-        To=[i,j]
-        addMove()
-    }
-}
-async function getChoice(){
-    let val = await new Promise(resolve=>{
-        choice = resolve;
-    })
-    choice = null;
-    return val;
-}
-
-function addMove(){
-    let moves;
-    console.log("add")
-    if(From&&To){
-        let number = Number(board[From[0]][From[1]].Value)
-        moves = [
-            {From:From,To:To,Number:number}
-        ]
-    }else if(From){
-        let number = Number(board[From[0]][From[1]].Value)
-        moves = [
-            {From:[-1,-1],To:From,Number:number}
-        ]
-    }else{
-        return
-    }
-    socketStore.makeMove(moves)
-    From = null
-    To = null
-    return
-}
+export let board;
+export let From;
+export let To;
+export let You;
+export let tdClick;
 
 function isHighlighted(i,j){
     if(From)
@@ -132,7 +45,6 @@ table td{
     background:#f687b3;
 }
 </style>
-<Choose resolve={choice}/>
 <table class="font-alloy text-lg table-fixed border-black border-4 rounded-lg bg-white">
     {#each board as row, i}
     <tr>
--- src/routes/game.svelte
+++ src/routes/game.svelte
@@ -1,11 +1,19 @@
 <script>
 import { goto } from '@sapper/app';
 import Table from "../components/Table.svelte";
+import Choose from "../components/Choose.svelte";
 import {socketStore} from "../store/websocket.js";
 
 let ink = 0
 let turn = "No"
 let jsonS=0;
+let board = [];
+let You = ""
+let From = null
+let To = null
+let choice = null
+let h=0,w=0;
+
 $: if($socketStore){
     if($socketStore.Command=="error"){
         alert(JSON.stringify($socketStore))
@@ -28,6 +36,100 @@ $:if($socketStore){
 async function show(arg){
     alert(arg)
 }
+$: console.log(JSON.stringify($socketStore))
+$: if($socketStore){
+    if($socketStore.Board&&$socketStore.Board.Board){
+        h = $socketStore.Board.Board.length;
+        if(h>0)
+            w = $socketStore.Board.Board[0].length;
+        makeBoard(h,w)
+        setBoard($socketStore.Board.Board)
+        
+    }
+        
+    You = $socketStore.You
+}
+
+function setBoard(boardData){
+    From = null
+    To = null
+    if(!boardData)
+        return
+    for(let i=0;i<h;i++){
+        for(let j=0;j<w;j++){
+            board[i][j] = boardData[i][j]
+        }
+    }
+}
+function makeBoard(h,w){
+    if((board.length==h)&&(board[0].length==w))
+        return
+    board = []
+    for(let i=0;i<h;i++){
+        let row = []
+        for(let j=0;j<w;j++){
+            row.push({Value:0,Owner:""})
+        }
+        board.push(row)
+    }
+}
+async function tdClick(i,j){
+    let cell = board[i][j]
+    if($socketStore.Next!=$socketStore.You)
+        return
+    if(cell.Value==-1)
+        return
+    if(!From){
+        if(cell.Owner!=You)
+            return
+        if(cell.Value==0){
+            From=[i,j]
+            let val = await getChoice()
+            if(val=="X"){
+                From = null
+                return
+            }
+            board[i][j].Value = Number(val)
+            addMove()
+            return
+        }
+        From = [i,j]
+        return
+    }else{
+        if((cell.Owner==You)&&(cell.Value>0))
+        return
+        To=[i,j]
+        addMove()
+    }
+}
+async function getChoice(){
+    let val = await new Promise(resolve=>{
+        choice = resolve;
+    })
+    choice = null;
+    return val;
+}
+
+function addMove(){
+    let moves;
+    if(From&&To){
+        let number = Number(board[From[0]][From[1]].Value)
+        moves = [
+            {From:From,To:To,Number:number}
+        ]
+    }else if(From){
+        let number = Number(board[From[0]][From[1]].Value)
+        moves = [
+            {From:[-1,-1],To:From,Number:number}
+        ]
+    }else{
+        return
+    }
+    socketStore.makeMove(moves)
+    From = null
+    To = null
+    return
+}
 </script>
 
 <style>
@@ -35,6 +137,7 @@ async function show(arg){
 <svelte:head>
 	<title>Match</title>
 </svelte:head>
+<Choose resolve={choice}/>
 <div class="hidden border-purple-300 border-white"></div>
 <div class="w-full h-full bg-black flex">
     <div
@@ -48,7 +151,7 @@ async function show(arg){
      class:border-purple-300={turn=='Yes'}
      class:border-white={turn=='No'}
      class="border-4 rounded-lg  flex">
-        <Table h=5 w=5/>
+        <Table board={board} From={From} To={To} You={You} tdClick={tdClick}/>
     </div>
     </div>
 </div>
